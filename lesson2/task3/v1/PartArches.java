9307803a8ab4a15308ca66caf905a345544ec71a
Hoolligan <gotti280985@gmail.com>
2017-09-20 18:57:06 +0300

    t3

---
+++ lesson2/task3/v1/PartArches.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Администратор on 20.09.2017.
+ */
+public class PartArches implements IProductPart {
+
+    public PartArches() {
+        System.out.println("Созданы Дужки");
+    }
+}
